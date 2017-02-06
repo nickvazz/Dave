3eb644c888dcf0a92bd7571972340a6bea5acd0e
Nick <nickvazquezz@gmail.com>
2017-02-05 17:13:01 -0800

    Updated to allow multiple runs for multiple Us

--- bottlenecks.py
+++ bottlenecks.py
@@ -3,102 +3,114 @@ import random
 import numpy as np
 import hubbard_input_data
 
-U = 20
-tempMin = .18
-tempMax = .22
+Us = [4,5,6,8,9,10,12,14,16,20]
+tempMins = [.18,.22,.29,.32,.38,.30,.29,.26,.22,.18]
+tempMaxs = [.22,.27,.34,.37,.42,.35,.34,.31,.27,.22]
 
-try:
-    data_file = 'Hubbard Data/N4x4x4_L200_U' + str(U) + '_Mu0/*'
-except:
-    data_file = '/home/kchng/Quantum Machine Learning/N4x4x4_L200_U' + str(U) + '_Mu0/*.stream'
 
-mnist = hubbard_input_data.dataAndLabels(data_file, tempMin=tempMin,tempMax=tempMax)
+for U in [8]:
+    for i in range(10):
 
-layer_trials = [[200,100,10,5]]
-batch_size = 10
-learning_rate = 0.1
-training_epochs = 1*1E2
+        tempRange = False
+        run_num = i + 1
+        run_str = 'run' + str(run_num) + '_U' + str(U) + '/'
 
-all_images = []
-all_labels = []
-for i in range(len(mnist.test.images)):
-    all_images.append(mnist.test.images[i])
-    all_labels.append(mnist.test.labels[i])
-for i in range(len(mnist.validation.images)):
-    all_images.append(mnist.validation.images[i])
-    all_labels.append(mnist.validation.labels[i])
-for i in range(len(mnist.train.images)):
-    all_images.append(mnist.train.images[i])
-    all_labels.append(mnist.train.labels[i])
+        try:
+            data_file = 'Hubbard Data/N4x4x4_L200_U' + str(U) + '_Mu0/*.stream'
+        except:
+            data_file = '/home/kchng/Quantum Machine Learning/N4x4x4_L200_U' + str(U) + '_Mu0/*.stream'
+            print('khatami_cluster')
 
-# print len(all_images)
+        if tempRange == True:
+            mnist = hubbard_input_data.dataAndLabels(data_file, tempMin=tempMin, tempMax=tempMax)
+        else:
+            mnist = hubbard_input_data.dataAndLabels(data_file)
 
-final_l1 = []
-final_l2 = []
-final_l3 = []
-final_l4 = []
-cant_find = []
-could_find = []
+        layer_trials = [[200,100,10,5]]
+        batch_size = 10
+        learning_rate = 0.1
+        training_epochs = 1*1E2
 
-r = random.random()
-random.shuffle(all_images, lambda:r)
-random.shuffle(all_labels, lambda:r)
+        all_images = []
+        all_labels = []
+        for i in range(len(mnist.test.images)):
+            all_images.append(mnist.test.images[i])
+            all_labels.append(mnist.test.labels[i])
+        for i in range(len(mnist.validation.images)):
+            all_images.append(mnist.validation.images[i])
+            all_labels.append(mnist.validation.labels[i])
+        for i in range(len(mnist.train.images)):
+            all_images.append(mnist.train.images[i])
+            all_labels.append(mnist.train.labels[i])
 
-try:
-    for layer in layer_trials:
-        # print layer
-        L1, L2, L3, L4 = layer
-        n_encoder_hidden_1 = L1
-        n_encoder_hidden_2 = L2
-        n_encoder_hidden_3 = L3
-        n_code = L4
+        # print len(all_images)
 
-        filename_p1 = str(n_encoder_hidden_1)+'_'+str(n_encoder_hidden_2)+'_'+str(n_encoder_hidden_3)+'_'
-        filename_p2 = str(n_code)+'_'+str(batch_size)+'_'+str(learning_rate)+'_'+str(int(training_epochs))
-        filename = filename_p1 + filename_p2
+        final_l1 = []
+        final_l2 = []
+        final_l3 = []
+        final_l4 = []
+        cant_find = []
+        could_find = []
 
-        L1W = np.loadtxt('U' + str(U) + '/' + 'layer_tensors/' + filename + '/L1W.txt')#, delimiter='')
-        L2W = np.loadtxt('U' + str(U) + '/' + 'layer_tensors/' + filename + '/L2W.txt')#, delimiter='')
-        L3W = np.loadtxt('U' + str(U) + '/' + 'layer_tensors/' + filename + '/L3W.txt')#, delimiter='')
-        L4W = np.loadtxt('U' + str(U) + '/' + 'layer_tensors/' + filename + '/L4W.txt')#, delimiter='')
-        L1b = np.loadtxt('U' + str(U) + '/' + 'layer_tensors/' + filename + '/L1b.txt')#, delimiter='')
-        L2b = np.loadtxt('U' + str(U) + '/' + 'layer_tensors/' + filename + '/L2b.txt')#, delimiter='')
-        L3b = np.loadtxt('U' + str(U) + '/' + 'layer_tensors/' + filename + '/L3b.txt')#, delimiter='')
-        L4b = np.loadtxt('U' + str(U) + '/' + 'layer_tensors/' + filename + '/L4b.txt')#, delimiter='')
+        r = random.random()
+        random.shuffle(all_images, lambda:r)
+        random.shuffle(all_labels, lambda:r)
 
-        for i in range(len(all_images)):
-            after_l1 = np.matmul(all_images[i],L1W)
-            after_l1 = np.add(after_l1, L1b)
-            final_l1.append(np.append(after_l1, all_labels[i]))
+        try:
+            for layer in layer_trials:
+                # print layer
+                L1, L2, L3, L4 = layer
+                n_encoder_hidden_1 = L1
+                n_encoder_hidden_2 = L2
+                n_encoder_hidden_3 = L3
+                n_code = L4
 
-            after_l2 = np.matmul(after_l1, L2W)
-            after_l2 = np.add(after_l2, L2b)
-            final_l2.append(np.append(after_l2, all_labels[i]))
+                filename_p1 = str(n_encoder_hidden_1)+'_'+str(n_encoder_hidden_2)+'_'+str(n_encoder_hidden_3)+'_'
+                filename_p2 = str(n_code)+'_'+str(batch_size)+'_'+str(learning_rate)+'_'+str(int(training_epochs))
+                filename = filename_p1 + filename_p2
 
-            after_l3 = np.matmul(after_l2, L3W)
-            after_l3 = np.add(after_l3, L3b)
-            final_l3.append(np.append(after_l3, all_labels[i]))
+                L1W = np.loadtxt(run_str + 'layer_tensors/' + filename + '/L1W.txt')#, delimiter='')
+                L2W = np.loadtxt(run_str + 'layer_tensors/' + filename + '/L2W.txt')#, delimiter='')
+                L3W = np.loadtxt(run_str + 'layer_tensors/' + filename + '/L3W.txt')#, delimiter='')
+                L4W = np.loadtxt(run_str + 'layer_tensors/' + filename + '/L4W.txt')#, delimiter='')
+                L1b = np.loadtxt(run_str + 'layer_tensors/' + filename + '/L1b.txt')#, delimiter='')
+                L2b = np.loadtxt(run_str + 'layer_tensors/' + filename + '/L2b.txt')#, delimiter='')
+                L3b = np.loadtxt(run_str + 'layer_tensors/' + filename + '/L3b.txt')#, delimiter='')
+                L4b = np.loadtxt(run_str + 'layer_tensors/' + filename + '/L4b.txt')#, delimiter='')
 
-            after_l4 = np.matmul(after_l3, L4W)
-            after_l4 = np.add(after_l4, L4b)
-            final_l4.append(np.append(after_l4, all_labels[i]))
+                for i in range(len(all_images)):
+                    after_l1 = np.matmul(all_images[i],L1W)
+                    after_l1 = np.add(after_l1, L1b)
+                    final_l1.append(np.append(after_l1, all_labels[i]))
 
-        if not os.path.exists('reduced_data/'+ filename):
-            os.makedirs('reduced_data/'+ filename)
+                    after_l2 = np.matmul(after_l1, L2W)
+                    after_l2 = np.add(after_l2, L2b)
+                    final_l2.append(np.append(after_l2, all_labels[i]))
 
-        np.savetxt('reduced_data/' + filename + '/AfterL1_' + filename + '.txt', final_l1, delimiter=' ', fmt='%10.5f')
-        np.savetxt('reduced_data/' + filename + '/AfterL2_' + filename + '.txt', final_l2, delimiter=' ', fmt='%10.5f')
-        np.savetxt('reduced_data/' + filename + '/AfterL3_' + filename + '.txt', final_l3, delimiter=' ', fmt='%10.5f')
-        np.savetxt('reduced_data/' + filename + '/AfterL4_' + filename + '.txt', final_l4, delimiter=' ', fmt='%10.5f')
+                    after_l3 = np.matmul(after_l2, L3W)
+                    after_l3 = np.add(after_l3, L3b)
+                    final_l3.append(np.append(after_l3, all_labels[i]))
 
-        could_find.append((L1,L2,L3,L4))
+                    after_l4 = np.matmul(after_l3, L4W)
+                    after_l4 = np.add(after_l4, L4b)
+                    final_l4.append(np.append(after_l4, all_labels[i]))
 
-        final_l4 = []; final_l3 = []; final_l2 = []; final_l1 = []
+                if not os.path.exists('reduced_data/'+ filename):
+                    os.makedirs('reduced_data/'+ filename)
 
-except:
-    cant_find.append((L1,L2,L3,L4))
+                np.savetxt(run_str + 'reduced_data/' + filename + '/AfterL1_' + filename + '.txt', final_l1, delimiter=' ', fmt='%10.5f')
+                np.savetxt(run_str + 'reduced_data/' + filename + '/AfterL2_' + filename + '.txt', final_l2, delimiter=' ', fmt='%10.5f')
+                np.savetxt(run_str + 'reduced_data/' + filename + '/AfterL3_' + filename + '.txt', final_l3, delimiter=' ', fmt='%10.5f')
+                np.savetxt(run_str + 'reduced_data/' + filename + '/AfterL4_' + filename + '.txt', final_l4, delimiter=' ', fmt='%10.5f')
 
-if len(cant_find) != 0:
-    print('cant find layers: ', cant_find)
+                could_find.append((L1,L2,L3,L4))
 
-os.system("python lowerDimensionEmbed.py")
+                final_l4 = []; final_l3 = []; final_l2 = []; final_l1 = []
+
+        except:
+            cant_find.append((L1,L2,L3,L4))
+
+        if len(cant_find) != 0:
+            print('cant find layers: ', cant_find)
+
+        # os.system("python lowerDimensionEmbed.py")
--- lowerDimensionEmbed.py
+++ lowerDimensionEmbed.py
@@ -1,6 +1,5 @@
 from time import time
 import os
-
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import offsetbox
@@ -9,138 +8,152 @@ from sklearn import (manifold, datasets, decomposition, ensemble,
 from StringIO import StringIO
 import random
 
-batch_size = 10
-learning_rate = 0.1
-training_epochs = 1*1E2
+Us = [4,5,6,8,9,10,12,14,16,20]
+tempMins = [.18,.22,.29,.32,.38,.30,.29,.26,.22,.18]
+tempMaxs = [.22,.27,.34,.37,.42,.35,.34,.31,.27,.22]
 
-random.seed(10)
-r = random.random()
 
-layer_trials = [[200,100,10,5]]
+for U in [8]
+    for i in range(10):
 
-for L1, L2, L3, L4 in layer_trials:
-    try:
-        for layer in [1,2,3,4]:
-            trial = str(L1) + '_' + str(L2) + '_' + str(L3) + '_' + str(L4) + '_' +str(batch_size)+'_'+str(learning_rate)+'_'+str(int(training_epochs))
-            embed_folder_path = 'embedded_data/' + trial + '/AfterL' + str(layer)
+        tempRange = False
+        run_num = i + 1
 
-            if not os.path.exists(embed_folder_path):
-                os.makedirs(embed_folder_path)
+        run_str = 'run' + str(run_num) + '_U' + str(U) + '/'
 
-            newX = []
-            newDataLabels = []
-            text2 = np.loadtxt('reduced_data/' + trial + '/AfterL' + str(layer) + '_' + trial + '.txt')
-            for i in range(len(text2)):
-                newX.append(text2[i][0:100])
-                newDataLabels.append(text2[i][-1])
 
-            random.shuffle(newX, lambda:r)
-            random.shuffle(newDataLabels, lambda:r)
+        batch_size = 10
+        learning_rate = 0.1
+        training_epochs = 1*1E2
 
-            X = newX
-            y = newDataLabels
-            print("\nLayer = %s" % layer)
-            #----------------------------------------------------------------------
-            # Scale and visualize the embedding vectors
-            def embedding(X):
-                x_min, x_max = np.min(X, 0), np.max(X, 0)
-                X = (X - x_min) / (x_max - x_min)
-                xs = []; ys = []
-                for i in range(len(X)):
-                    xs.append(X[i][0])
-                    ys.append(X[i][1])
-                return (xs, ys)
+        random.seed(10)
+        r = random.random()
 
-            def plotting(xs, ys, title, a, b):
-                # plt.subplot(1,3,a)
-                plt.subplot(1,2,a) # no t-SNE
-                plt.scatter(xs,ys, c=y, cmap=plt.cm.get_cmap("plasma",50))
-                plt.colorbar(ticks=range(0,2,1))
-                plt.xticks([]), plt.yticks([])
-                if title is not None:
-                    plt.title(title)
+        layer_trials = [[200,100,10,5]]
 
-            # plt.style.use('bmh')
-            # plt.style.use('ggplot')
-            # plt.style.use('dark_background')
-            plt.figure(figsize=(30,10))
+        for L1, L2, L3, L4 in layer_trials:
+            try:
+                for layer in [1,2,3,4]:
+                    trial = str(L1) + '_' + str(L2) + '_' + str(L3) + '_' + str(L4) + '_' +str(batch_size)+'_'+str(learning_rate)+'_'+str(int(training_epochs))
+                    embed_folder_path = run_str + 'embedded_data/' + trial + '/AfterL' + str(layer)
 
-            # Projection on to the first 2 principal components
-            print("Computing PCA projection")
-            t0 = time()
-            X_pca = decomposition.TruncatedSVD(n_components=2).fit_transform(X)
-            xs, ys = embedding(X_pca)
-            np.savetxt(embed_folder_path + '/pca.txt', (xs,ys,y), fmt='%10.5f')
-            # plotting(xs,ys,"PCA",1,4) # with hist
-            plotting(xs,ys,"PCA",1,50) # no hist
-            print ("(time %.2fs)" % (time() - t0))
+                    if not os.path.exists(embed_folder_path):
+                        os.makedirs(embed_folder_path)
 
-            #
-            # Random Trees embedding of the digits dataset
-            print("Computing Totally Random Trees embedding")
-            # hasher = ensemble.RandomTreesEmbedding(n_estimators=1000, random_state=0, max_depth=10, n_jobs=-1, verbose=3)
-            hasher = ensemble.RandomTreesEmbedding(n_estimators=1000, random_state=0, max_depth=10, n_jobs=-1)
-            t0 = time()
-            X_transformed = hasher.fit_transform(X)
-            pca = decomposition.TruncatedSVD(n_components=2)
-            X_reduced = pca.fit_transform(X_transformed)
-            xs, ys = embedding(X_reduced)
-            np.savetxt(embed_folder_path + '/randTrees.txt', (xs,ys,y), fmt='%10.5f')
-            # plotting(xs,ys,"Random Forest",7,10) # with hist
-            plotting(xs,ys,"Random Forest",2,50) # no hist
-            print("(time %.2fs)" %(time() - t0))
+                    newX = []
+                    newDataLabels = []
+                    text2 = np.loadtxt(run_str + 'reduced_data/' + trial + '/AfterL' + str(layer) + '_' + trial + '.txt')
+                    for i in range(len(text2)):
+                        newX.append(text2[i][0:100])
+                        newDataLabels.append(text2[i][-1])
 
-            # # Spectral embedding of the digits dataset
-            # print("Computing Spectral embedding")
-            # embedder = manifold.SpectralEmbedding(n_components=2, random_state=0, eigen_solver="arpack")
-            # t0 = time()
-            # X_se = embedder.fit_transform(X)
-            # xs, ys = embedding(X_se)
-            # np.savetxt(embed_folder_path + 'spectral.txt', (xs,ys,y), fmt='%10.5f')
-            # # plotting(xs,ys,"Spectral",8,11) # with hist
-            # plotting(xs,ys,"Spectral",5,50) # no hist
-            # print("(time %.2fs)" % (time() - t0))
+                    random.shuffle(newX, lambda:r)
+                    random.shuffle(newDataLabels, lambda:r)
 
-            # # t-SNE embedding of the digits dataset
-            # print("Computing t-SNE embedding")
-            # tsne = manifold.TSNE(n_components=2, init='pca', random_state=0, n_iter=1000, verbose=4)
-            # t0 = time()
-            # X_tsne = tsne.fit_transform(X)
-            # xs, ys = embedding(X_tsne)
-            # np.savetxt(embed_folder_path + '/tsne.txt', (xs,ys,y), fmt='%10.5f')
-            # # plotting(xs,ys,"t-SNE",9,12) # with hist
-            # plotting(xs,ys,"t-SNE",3,50) # no hist
-            # print("(time %.2fs)" % (time() - t0))
+                    X = newX
+                    y = newDataLabels
+                    print("\nLayer = %s" % layer)
+                    #----------------------------------------------------------------------
+                    # Scale and visualize the embedding vectors
+                    def embedding(X):
+                        x_min, x_max = np.min(X, 0), np.max(X, 0)
+                        X = (X - x_min) / (x_max - x_min)
+                        xs = []; ys = []
+                        for i in range(len(X)):
+                            xs.append(X[i][0])
+                            ys.append(X[i][1])
+                        return (xs, ys)
 
-            # # # Isomap projection of the digits dataset
-            # print("Computing Isomap embedding")
-            # t0 = time()
-            # n_neighbors = 30
-            # X_iso = manifold.Isomap(n_neighbors,n_components=2, n_jobs=-1).fit_transform(X)
-            # xs, ys = embedding(X_iso)
-            # np.savetxt(embed_folder_path + '_isomap.txt', (xs,ys,y), fmt='%10.5f')
-            # # plotting(xs,ys,"Isomap",2,5) # with hist
-            # plotting(xs,ys,"Isomap",4,50) # no hist
-            # print("(time %.2fs)" % (time() - t0))
+                    def plotting(xs, ys, title, a, b):
+                        # plt.subplot(1,3,a)
+                        plt.subplot(1,2,a) # no t-SNE
+                        plt.scatter(xs,ys, c=y, cmap=plt.cm.get_cmap("plasma",50))
+                        plt.colorbar(ticks=range(0,2,1))
+                        plt.xticks([]), plt.yticks([])
+                        if title is not None:
+                            plt.title(title)
 
-            # # MDS  embedding of the digits dataset
-            # print("Computing MDS embedding")
-            # clf = manifold.MDS(n_components=2, n_init=1, max_iter=1000, n_jobs=-1, verbose=4)
-            # t0 = time()
-            # X_mds = clf.fit_transform(X)
-            # print("Stress: %f" % clf.stress_)
-            # xs, ys = embedding(X_mds)
-            # np.savetxt(embed_folder_path + '_mds.txt', (xs,ys,y), fmt='%10.5f')
-            # plotting(xs,ys,"MDS",6,50) # no hist
-            # # plotting(xs,ys,"MDS",3,6) # with hist
-            # print("(time %.2fs)" % (time() - t0))
+                    # plt.style.use('bmh')
+                    # plt.style.use('ggplot')
+                    # plt.style.use('dark_background')
+                    plt.figure(figsize=(30,10))
 
-            if not os.path.exists('layer_figures/' + trial):
-                os.makedirs('layer_figures/' + trial)
-            plt.savefig('layer_figures/' + trial + '/layer' + str(layer) + '_' + str(len(X)) +'.png')
-            plt.clf()
+                    # Projection on to the first 2 principal components
+                    print("Computing PCA projection")
+                    t0 = time()
+                    X_pca = decomposition.TruncatedSVD(n_components=2).fit_transform(X)
+                    xs, ys = embedding(X_pca)
+                    np.savetxt(embed_folder_path + '/pca.txt', (xs,ys,y), fmt='%10.5f')
+                    # plotting(xs,ys,"PCA",1,4) # with hist
+                    plotting(xs,ys,"PCA",1,50) # no hist
+                    print ("(time %.2fs)" % (time() - t0))
 
-    except:
-        print('failed to find layer', L1,L2,L3,L4)
-# os.system('python classificationEmbed.py')
-os.system("python fanFitting.py")
+                    #
+                    # Random Trees embedding of the digits dataset
+                    print("Computing Totally Random Trees embedding")
+                    # hasher = ensemble.RandomTreesEmbedding(n_estimators=1000, random_state=0, max_depth=10, n_jobs=-1, verbose=3)
+                    hasher = ensemble.RandomTreesEmbedding(n_estimators=1000, random_state=0, max_depth=10, n_jobs=-1)
+                    t0 = time()
+                    X_transformed = hasher.fit_transform(X)
+                    pca = decomposition.TruncatedSVD(n_components=2)
+                    X_reduced = pca.fit_transform(X_transformed)
+                    xs, ys = embedding(X_reduced)
+                    np.savetxt(embed_folder_path + '/randTrees.txt', (xs,ys,y), fmt='%10.5f')
+                    # plotting(xs,ys,"Random Forest",7,10) # with hist
+                    plotting(xs,ys,"Random Forest",2,50) # no hist
+                    print("(time %.2fs)" %(time() - t0))
+
+                    # # Spectral embedding of the digits dataset
+                    # print("Computing Spectral embedding")
+                    # embedder = manifold.SpectralEmbedding(n_components=2, random_state=0, eigen_solver="arpack")
+                    # t0 = time()
+                    # X_se = embedder.fit_transform(X)
+                    # xs, ys = embedding(X_se)
+                    # np.savetxt(embed_folder_path + 'spectral.txt', (xs,ys,y), fmt='%10.5f')
+                    # # plotting(xs,ys,"Spectral",8,11) # with hist
+                    # plotting(xs,ys,"Spectral",5,50) # no hist
+                    # print("(time %.2fs)" % (time() - t0))
+
+                    # # t-SNE embedding of the digits dataset
+                    # print("Computing t-SNE embedding")
+                    # tsne = manifold.TSNE(n_components=2, init='pca', random_state=0, n_iter=1000, verbose=4)
+                    # t0 = time()
+                    # X_tsne = tsne.fit_transform(X)
+                    # xs, ys = embedding(X_tsne)
+                    # np.savetxt(embed_folder_path + '/tsne.txt', (xs,ys,y), fmt='%10.5f')
+                    # # plotting(xs,ys,"t-SNE",9,12) # with hist
+                    # plotting(xs,ys,"t-SNE",3,50) # no hist
+                    # print("(time %.2fs)" % (time() - t0))
+
+                    # # # Isomap projection of the digits dataset
+                    # print("Computing Isomap embedding")
+                    # t0 = time()
+                    # n_neighbors = 30
+                    # X_iso = manifold.Isomap(n_neighbors,n_components=2, n_jobs=-1).fit_transform(X)
+                    # xs, ys = embedding(X_iso)
+                    # np.savetxt(embed_folder_path + '_isomap.txt', (xs,ys,y), fmt='%10.5f')
+                    # # plotting(xs,ys,"Isomap",2,5) # with hist
+                    # plotting(xs,ys,"Isomap",4,50) # no hist
+                    # print("(time %.2fs)" % (time() - t0))
+
+                    # # MDS  embedding of the digits dataset
+                    # print("Computing MDS embedding")
+                    # clf = manifold.MDS(n_components=2, n_init=1, max_iter=1000, n_jobs=-1, verbose=4)
+                    # t0 = time()
+                    # X_mds = clf.fit_transform(X)
+                    # print("Stress: %f" % clf.stress_)
+                    # xs, ys = embedding(X_mds)
+                    # np.savetxt(embed_folder_path + '_mds.txt', (xs,ys,y), fmt='%10.5f')
+                    # plotting(xs,ys,"MDS",6,50) # no hist
+                    # # plotting(xs,ys,"MDS",3,6) # with hist
+                    # print("(time %.2fs)" % (time() - t0))
+
+                    if not os.path.exists(run_str + 'layer_figures/' + trial):
+                        os.makedirs(run_str + 'layer_figures/' + trial)
+                    plt.savefig(run_str + 'layer_figures/' + trial + '/layer' + str(layer) + '_' + str(len(X)) +'.png')
+                    plt.clf()
+
+            except:
+                print('failed to find layer', L1,L2,L3,L4)
+        # os.system('python classificationEmbed.py')
+        # os.system("python fanFitting.py")
